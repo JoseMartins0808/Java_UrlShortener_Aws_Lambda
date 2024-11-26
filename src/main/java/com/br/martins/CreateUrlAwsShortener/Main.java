6565ee2775f15fa7de674f6d184e4fb944d8f38f
JoseMartins0808 <ottenazuose@hotmail.com>
2024-11-26 12:43:04 -0300

    feat: extrating url parameters

--- src/main/java/com/br/martins/CreateUrlAwsShortener/Main.java
+++ src/main/java/com/br/martins/CreateUrlAwsShortener/Main.java
@@ -1,15 +1,36 @@
 package com.br.martins.CreateUrlAwsShortener;
 
 import java.util.Map;
+import java.util.UUID;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
+    final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
-        // NÃO ESQUEÇA de criar um repositório no git hub
-        throw new UnsupportedOperationException("Unimplemented method 'handleRequest'");
+
+        final String body = input.get("body").toString();
+
+        Map <String, String> bodyMap;
+
+        try{
+
+            bodyMap = objectMapper.readValue(body, Map.class);
+
+        }catch(Exception exception) {
+
+            throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
+        }
+
+        final String originalUrl = bodyMap.get("originalUrl");
+        final String expirationTime = bodyMap.get("expirationTime");
+        final String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        return null;
     }
 }
