c1be3ec38fd84b2010e1100ed43f44f2e0009f18
JoseMartins0808 <ottenazuose@hotmail.com>
2024-11-26 22:46:40 -0300

    feat: build Aws Lambda function config

--- src/main/java/com/br/martins/CreateUrlAwsShortener/Main.java
+++ src/main/java/com/br/martins/CreateUrlAwsShortener/Main.java
@@ -1,5 +1,6 @@
 package com.br.martins.CreateUrlAwsShortener;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -31,6 +32,10 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
         final String expirationTime = bodyMap.get("expirationTime");
         final String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
 
-        return null;
+        final Map<String, String> response = new HashMap<String,String>();
+        response.put("code", shortUrlCode);
+
+
+        return response;
     }
 }
