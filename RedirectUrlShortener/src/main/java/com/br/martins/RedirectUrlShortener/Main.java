fe27f0bde3beeb4c170878919dda813210057cad
JoseMartins0808 <ottenazuose@hotmail.com>
2024-12-04 00:01:48 -0300

    feat: s3 client at redirect function

--- RedirectUrlShortener/src/main/java/com/br/martins/RedirectUrlShortener/Main.java
+++ RedirectUrlShortener/src/main/java/com/br/martins/RedirectUrlShortener/Main.java
@@ -1,7 +1,25 @@
 package com.br.martins.RedirectUrlShortener;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import software.amazon.awssdk.services.s3.S3Client;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private final S3Client s3Client = S3Client.builder().build();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        
+        final String pathParameter = input.get("rawPath").toString(); // em uma URL http://site.com/UUID, extrairá o seguinte: "/UUID"
+        final String urlCode = pathParameter.replace("/", "");
+
+        if(urlCode == null || urlCode.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'shortUrlCode is required.'");
+        }
+        return null;
     }
 }
